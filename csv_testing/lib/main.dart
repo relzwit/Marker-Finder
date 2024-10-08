fe89ec92cc908bc2c9275d2186c00dad6dfc888c
Kellen Erskine <kellen@Kellens-MacBook-Pro.local>
2024-10-08 13:34:48 -0400

    couple lines changed

--- csv_testing/lib/main.dart
+++ csv_testing/lib/main.dart
@@ -3,37 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
 
-//coordinates for SAU: 35.04842984003839, -85.05191851568703
+// find a flutter component for a map display
+
+
+// coordinates for SAU: 35.04842984003839, -85.05191851568703
 
 void main() {
   runApp(const MyApp());
 }
 
-// //uses the haversine formula to check distance between two coords
-// //-------------------------------------------------------------
-// class Haversine {
-//   static final R = 6372.8; // In kilometers
-
-//   static double haversine(double lat1, lon1, lat2, lon2) {
-//     double dLat = _toRadians(lat2 - lat1);
-//     double dLon = _toRadians(lon2 - lon1);
-//     lat1 = _toRadians(lat1);
-//     lat2 = _toRadians(lat2);
-//     double a =
-//         pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat1) * cos(lat2);
-//     double c = 2 * asin(sqrt(a));
-//     return R * c;
-//   }
-
-//   static double _toRadians(double degree) {
-//     return degree * pi / 180;
-//   }
-
-//   static void main() {
-//     print(haversine(36.12, -86.67, 33.94, -118.40));
-//   }
-// }
-// //-------------------------------------------------------------
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -71,7 +49,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<List<dynamic>> _data = [];
+  List<List<dynamic>> _data = []; 
   List<List<dynamic>> _closeLocations = [];
 
   static final R = 6372.8; // In kilometers
