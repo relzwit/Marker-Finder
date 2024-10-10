12ba4b3f00f8727fe2b769dca51c37f266b2c2df
Kellen Erskine <kellen@Kellens-MacBook-Pro.local>
2024-10-10 13:47:05 -0400

    stuff

--- location_testing/lib/main.dart
+++ location_testing/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:math';
+import 'package:geolocator/geolocator.dart';
 
 // find a flutter component for a map display
 
@@ -9,7 +10,44 @@ import 'dart:math';
 
 void main() {
   runApp(const MyApp());
-  runApp(const LocationPage());
+  _determinePosition();
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
 }
 
 class MyApp extends StatelessWidget {
@@ -29,22 +67,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyLocationPage extends StatelessWidget {
-  const MyLocationPage({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Location',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyLocationPage(title: 'Read from CSV test'),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -68,6 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
   List<List<dynamic>> _data = [];
   List<List<dynamic>> _closeLocations = [];
 
+
+  //  haversine stuff to compare two sets of coords
   static final R = 6372.8; // In kilometers
 
   static double haversine(double lat1, lon1, lat2, lon2) {
@@ -85,6 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return degree * pi / 180;
   }
 
+  //  code to load the info from the CSV file into a list
   void _loadCSV() async {
     final _rawData = await rootBundle.loadString("assets/hmdb.csv");
     List<List<dynamic>> _listData =
