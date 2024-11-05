06d820f8f8484870165568b692f64ebd8520ad59
Kellen Erskine <kellenae129@gmail.com>
2024-11-05 13:24:45 -0500

    deleted a folder

--- fluttermap_markerpopup-main/lib/src/animated_stack_widget.dart
+++
@@ -1,4 +0,0 @@
-library animated_stack_widget;
-
-export 'animated_stack.dart';
-export 'animated_stack_manager.dart';
--- fluttermap_markerpopup-main/lib/src/markerdata.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_map/flutter_map.dart';
-
-abstract class MarkerData {
-  Marker marker;
-  MarkerData({required this.marker});
-}
